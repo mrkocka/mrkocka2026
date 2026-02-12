ec5a66e5748b21a239b3f89dceaaf13c8205c6d6
mrkocka <radics.pepe@gmail.com>
2026-02-13 00:52:34 +0100

    EJS mode

--- server.js
+++ server.js
@@ -3,20 +3,31 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// 1. Statikus fájlok kiszolgálása kiterjesztés nélkül (Szép URL-ek)
-// Az 'extensions' opcióval az Express megpróbálja .html-ként is keresni a fájlt
-app.use(
-  express.static(path.join(__dirname, "public"), {
-    extensions: ["html", "htm"],
-  }),
-);
+// Beállítjuk az EJS-t sablonmotornak
+app.set("view engine", "ejs");
 
-// 2. 404-es hibaoldal kezelése
-// Ez a rész csak akkor fut le, ha a fenti sor nem talált illeszkedő fájlt
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+// A statikus fájlok (CSS, képek) továbbra is a public-ból jönnek
+app.use(express.static("public"));
+
+// 1. A főoldal kezelése (amikor nincs semmi a perjel után)
+app.get("/", (req, res) => {
+  res.render("index", (err, html) => {
+    if (err) return res.status(404).render("404");
+    res.send(html);
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`A szerver száguld: http://localhost:${PORT}`);
+// 2. Minden más oldal kezelése
+app.get("/:page", (req, res) => {
+  const page = req.params.page;
+
+  res.render(page, (err, html) => {
+    if (err) {
+      // Ha nem találja a fájlt (pl. rossz URL), jöhet a 404
+      return res.status(404).render("404");
+    }
+    res.send(html);
+  });
 });
+
+app.listen(PORT, () => console.log(`Szerver: http://localhost:${PORT}`));
